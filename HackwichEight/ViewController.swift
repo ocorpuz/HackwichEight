788f353303604ee6ddbd4f82b4576400867ef9f3
ocorpuz <59846122+ocorpuz@users.noreply.github.com>
2020-03-12 10:22:22 -1000

    Part 3 1-5 completed

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -13,19 +13,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 0
+    
+    //part 2 of Part 3
+    var targetValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        
+        startNewRound()
     }
     
     
     @IBAction func GuessNumberPressed(_ sender: Any) {
-        let message = "The value is:\(currentValue)"
+        let message = "The value is:\(currentValue)" + "\nThe target value is:\(targetValue)"
         let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
         
         
     }
@@ -35,8 +43,10 @@ class ViewController: UIViewController {
         print("The number is:\(slider.value)")
         currentValue = lroundf(slider.value)
         
-
-        
+        }
+    func startNewRound(){
+        targetValue = Int.random(in: 0...100)
+        currentValue = lroundf(slider.value)
     }
     
     
